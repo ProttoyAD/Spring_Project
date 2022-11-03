8f2bcc29cda933a39a558a4210273aedb29df2c0
Shyamol-Mohanto <shyamolmohanto26@gmail.com>
2022-11-03 10:46:59 +0600

    project

--- src/main/java/com/example/Project/service/ProductService.java
+++ src/main/java/com/example/Project/service/ProductService.java
@@ -7,11 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ProductService {
     @Autowired
     ProductRepository productRepository;
-    public List<Product> getAllProduct(){
-        return productRepository.findAll();
+    public List<Product> getAllProduct(){return productRepository.findAll();}
+    public void addProduct(Product product){
+        productRepository.save(product);
     }
 }
