4e5aba62ce04d80743de3e03dc623c20872dd11f
h-dolon <hosnearadolon@gmail.com>
2022-11-06 21:39:52 +0600

    Project

---
+++ src/main/java/com/example/Project/controller/HomeController.java
@@ -0,0 +1,47 @@
+package com.example.Project.controller;
+
+import com.example.Project.service.CategoryService;
+import com.example.Project.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HomeController {
+    @Autowired
+    CategoryService categoryService;
+    @Autowired
+    ProductService productService;
+
+    @GetMapping({"/", "/home"})
+    public String home(Model model){
+        return "index";
+
+    }
+    @GetMapping({"/shop"})
+    public String shop(Model model){
+        model.addAttribute(" categories", categoryService.getAllCategory());
+        model.addAttribute("products", productService.getAllProduct());
+
+        return "shop";
+
+    }
+    @GetMapping({"/shop/category/{id}"})
+    public String shopByCategory(Model model, @PathVariable int id){
+        model.addAttribute(" categories", categoryService.getAllCategory());
+        model.addAttribute("products", productService.getAllProductsByCategoryId(id));
+
+        return "shop";
+
+    }
+    @GetMapping({"/shop/viewproduct/{id}"})
+    public String viewProduct(Model model, @PathVariable int id){
+        model.addAttribute(" product", productService.getProductById(id).get());
+
+
+        return "viewProduct";
+
+    }
+}
